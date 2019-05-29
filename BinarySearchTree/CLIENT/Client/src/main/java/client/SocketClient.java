06e4a231070be67ce4dc457480d270e165c04d80
Erthax <mateuszkochanek01@gmail.com>
2019-05-29 21:34:53 +0200

    #09.Choosing options added

--- BinarySearchTree/CLIENT/Client/src/main/java/client/SocketClient.java
+++ BinarySearchTree/CLIENT/Client/src/main/java/client/SocketClient.java
@@ -6,32 +6,62 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class SocketClient {
-    Socket socket = null;
-    PrintWriter out = null;
-    BufferedReader in = null;
+	Socket socket = null;
+	PrintWriter out = null;
+	BufferedReader in = null;
+	int option;
+	Scanner input = new Scanner(System.in);
 
-    public SocketClient(int type){
-      listenSocket();
-      printinstructions ();
-      out.println(type);
-    }
- 
-    private void printinstructions (){
-      System.out.println("Instrukcje jakie mozesz wykonać na drzewie to: (wybierz 1, 2, 3 lub 4)");
-      System.out.println("1.Dodaj\n2.Usun\n3.Narysuj\n4.Koniec");
-    }
+	public SocketClient(int type) {
+		listenSocket();
+		out.println(type);
+		try {
+			System.out.print(in.readLine());
+		} catch (IOException ex) {
+			System.out.print("nie pyklo");
+		}
+		printInstructions();
+		sendInstructions();
 
-    public void listenSocket(){
-        try {
-          socket = new Socket("localhost", 4444);
-          out = new PrintWriter(socket.getOutputStream(), true);
-          in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        } catch (UnknownHostException e) {
-           System.out.println("Unknown host: localhost"); System.exit(1);
-        } catch  (IOException e) {
-           System.out.println("Brak I/O"); System.exit(1);
-        }
-    }
+	}
+
+	private void printInstructions() {
+		System.out.println("\nInstrukcje jakie mozesz wykonać na drzewie to: (wybierz 1, 2, 3 lub 4)");
+		System.out.println("1.Dodaj\n2.Usun\n3.Narysuj\n4.Koniec");
+	}
+
+	private void sendInstructions() {
+		do{
+			try {
+                option = input.nextInt();
+                if (option == 1 || option == 2 || option == 3 || option == 4){
+					out.println(option);
+				}
+                else
+                    System.out.println("Proszę wpisać 1,2,3 lub 4");
+            } catch (InputMismatchException e) {
+                System.out.println("zły typ danych, proszę wpisać 1,2,3 lub 4");
+                input.next();
+            }
+
+		}while(option != 4);
+	}
+
+	public void listenSocket() {
+		try {
+			socket = new Socket("localhost", 4444);
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host: localhost");
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("Brak I/O");
+			System.exit(1);
+		}
+	}
 }
--- BinarySearchTree/SERVER/Server/src/main/java/server/ServerMain.java
+++ BinarySearchTree/SERVER/Server/src/main/java/server/ServerMain.java
@@ -11,62 +11,100 @@ import binarytree.BinaryTree;
 
 public class ServerMain 
 {
-    ServerSocket server = null;
-    Socket client = null;
-    BufferedReader in = null;
-    PrintWriter out = null;
-    String line = "";
+    private ServerSocket server = null;
+    private  Socket client = null;
+    private BufferedReader in = null;
+    private PrintWriter out = null;
+    private String line = "";
+    private BinaryTree tree;
+    private String option = "";
+    private String value = "";
   
     ServerMain() { 
-      try {
-        server = new ServerSocket(4444); 
-      } 
-      catch (IOException e) {
-        System.out.println("Could not listen on port 4444"); System.exit(-1);
-      }
+        try {
+            server = new ServerSocket(4444); 
+        } catch (IOException e) {
+            System.out.println("Could not listen on port 4444"); System.exit(-1);
+        }
+        listenSocket();
+        createTree();
+        getInstructions();
     }
   
     public void listenSocket() {
-      try {
-        client = server.accept();
-      } 
-      catch (IOException e) {
-        System.out.println("Accept failed: 4444"); System.exit(-1);
-      }
-      try {
-        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        out = new PrintWriter(client.getOutputStream(), true);
-      } 
-      catch (IOException e) {
-        System.out.println("Accept failed: 4444"); System.exit(-1);
-      }
-      while(line != null) {
         try {
-          line = in.readLine();
-          System.out.println(line);
-          out.println("-> ("+line+")");
-        } 
-        catch (IOException e) {
-          System.out.println("Read failed"); System.exit(-1);
-        } 
-      }
+            client = server.accept();
+        } catch (IOException e) {
+            System.out.println("Accept failed: 4444"); System.exit(-1);
+        }
+        try {
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintWriter(client.getOutputStream(), true);
+        } catch (IOException e) {
+            System.out.println("in and out failed"); System.exit(-1);
+        }
     }
-  
+
+    private void createTree(){
+        try {
+            String type = in.readLine();
+            if(type.compareTo("1") == 0 )
+                tree = new BinaryTree<Integer>();
+            else if(type.compareTo("2") == 0 )
+                tree = new BinaryTree<Double>();
+            else if(type.compareTo("3") == 0 )
+                tree = new BinaryTree<String>();
+            out.println("Stworzono drzewo");
+        } catch (IOException e) {
+            System.out.println("Read failed"); System.exit(-1);
+        }
+    }
+
+    private void getInstructions(){
+        do{
+            try {
+                option = in.readLine();
+                System.out.print("tu wbiłem");
+                if(option.compareTo("1") == 0 ){
+                    System.out.print("wybrano insert");
+                } else if(option.compareTo("2") == 0 ) {
+                    System.out.print("wybrano delete");
+                }else if(option.compareTo("3") == 0 ){
+                    System.out.print("wybrano draw");
+                }else if(option.compareTo("4") == 0 ){
+                    System.out.print("wybrano wyjscie");
+                }
+            } catch (IOException e) {
+                System.out.println("Read failed"); System.exit(-1);
+            }
+
+        }while(option.compareTo("4") != 0);
+
+    }
+
     protected void finalize() {
-      try {
-        in.close();
-        out.close();
-        client.close();
-        server.close();
-      } 
-      catch (IOException e) {
-        System.out.println("Could not close."); System.exit(-1);
-      }
+        try {
+            in.close();
+            out.close();
+            client.close();
+            server.close();
+        } catch (IOException e) {
+            System.out.println("Could not close."); System.exit(-1);
+        }
     }
   
     public static void main(String[] args) {
-    ServerMain server = new ServerMain();
-      server.listenSocket();
+        ServerMain server = new ServerMain();
     }
     
 }
+/*if (option == 1){
+
+} else if(option == 2){
+
+} else if(option == 3){
+
+} else if(option == 4){
+
+} else
+    System.out.println("Proszę wpisać 1,2,3 lub 4");*/
