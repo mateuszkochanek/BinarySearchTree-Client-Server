efcb7f116af02532bf69e61b12bc42ec25eee6c7
Erthax <mateuszkochanek01@gmail.com>
2019-05-29 19:28:44 +0200

    #08.Basic Server operations added (again)

--- BinarySearchTree/SERVER/Server/src/main/java/server/ServerMain.java
+++ BinarySearchTree/SERVER/Server/src/main/java/server/ServerMain.java
@@ -1,33 +1,72 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import binarytree.BinaryTree;
 
 public class ServerMain 
 {
-    public static void main( String[] args )
-    {
-        BinaryTree<Integer> binaryTree = new BinaryTree<Integer>();
-        binaryTree.insert(50);
-        binaryTree.insert(25);
-        binaryTree.insert(75);
-        binaryTree.insert(15);
-        binaryTree.insert(7);
-        binaryTree.insert(22);
-        binaryTree.insert(65);
-        binaryTree.insert(85);
-        binaryTree.insert(58);
-        binaryTree.insert(72);
-        binaryTree.insert(79);
-        binaryTree.insert(92);
-        binaryTree.insert(35);
-        binaryTree.insert(27);
-        binaryTree.insert(42);
-        binaryTree.draw();
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
-        binaryTree.delete(75);
-        binaryTree.draw();
-
+    ServerSocket server = null;
+    Socket client = null;
+    BufferedReader in = null;
+    PrintWriter out = null;
+    String line = "";
+  
+    ServerMain() { 
+      try {
+        server = new ServerSocket(4444); 
+      } 
+      catch (IOException e) {
+        System.out.println("Could not listen on port 4444"); System.exit(-1);
+      }
     }
+  
+    public void listenSocket() {
+      try {
+        client = server.accept();
+      } 
+      catch (IOException e) {
+        System.out.println("Accept failed: 4444"); System.exit(-1);
+      }
+      try {
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        out = new PrintWriter(client.getOutputStream(), true);
+      } 
+      catch (IOException e) {
+        System.out.println("Accept failed: 4444"); System.exit(-1);
+      }
+      while(line != null) {
+        try {
+          line = in.readLine();
+          System.out.println(line);
+          out.println("-> ("+line+")");
+        } 
+        catch (IOException e) {
+          System.out.println("Read failed"); System.exit(-1);
+        } 
+      }
+    }
+  
+    protected void finalize() {
+      try {
+        in.close();
+        out.close();
+        client.close();
+        server.close();
+      } 
+      catch (IOException e) {
+        System.out.println("Could not close."); System.exit(-1);
+      }
+    }
+  
+    public static void main(String[] args) {
+    ServerMain server = new ServerMain();
+      server.listenSocket();
+    }
+    
 }
